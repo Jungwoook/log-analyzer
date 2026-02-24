c29861b54d398035c4c3809a09f0ab76e66fd934
Jungwoook <okjungwoo@gmail.com>
2026-02-24 17:17:36 +0900

    feat: project init

---
+++ src/test/java/com/jw/log_analyzer/LogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.log_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
