0cf441bc37653fd9e1425187fde6a1246b523dcd
Jungwoook <okjungwoo@gmail.com>
2026-03-17 17:00:40 +0900

    refactor: lombok 반영하여 annotaion 수정

--- src/main/java/com/jw/log_analyzer/dto/AnalysisResultDto.java
+++ src/main/java/com/jw/log_analyzer/dto/AnalysisResultDto.java
@@ -1,21 +1,15 @@
 package com.jw.log_analyzer.dto;
 
+import lombok.Value;
+
 import java.util.List;
+import java.util.Map;
 
+@Value
 public class AnalysisResultDto {
-    private final String mostCalledApiKey;
-    private final List<TopServiceDto> top3Services;
-    private final java.util.Map<String, Double> browserRatio;
-
-    public AnalysisResultDto(String mostCalledApiKey, List<TopServiceDto> top3Services, java.util.Map<String, Double> browserRatio) {
-        this.mostCalledApiKey = mostCalledApiKey;
-        this.top3Services = top3Services;
-        this.browserRatio = browserRatio;
-    }
-
-    public String getMostCalledApiKey() { return mostCalledApiKey; }
-    public List<TopServiceDto> getTop3Services() { return top3Services; }
-    public java.util.Map<String, Double> getBrowserRatio() { return browserRatio; }
+    String mostCalledApiKey;
+    List<TopServiceDto> top3Services;
+    Map<String, Double> browserRatio;
 
     public record TopServiceDto(String serviceId, long count) {
     }
